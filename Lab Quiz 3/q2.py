5179af418d3aa9041fb7706b3da6e9b86897726b
“Abbas <a.rizvi7@share.epsb.ca>
2024-04-08 21:58:32 -0600

    lab quiz and assignments

--- Lab Quiz 3/q2.py
+++ Lab Quiz 3/q2.py
@@ -9,11 +9,7 @@ def checkEqual():
         row = input('')
         board.append(list(row)) # board is a 2D list of rows/columns
         set_list.append((set(board[i]))) # a list of unique terms in each line in set form
-    '''
-    for i in range(len(board)):
-        if len(board[i]) != row_column[1]:
-            return False
-    '''
+
     # check to see to each element in set list is the same
     for i in range(len(set_list)):
         if set_list[0] == set_list[i]:
@@ -27,7 +23,7 @@ def checkAdj():
     for i in range(len(board)):
         for j in range(len(board[i])):
             try:
-                if board[i][j] == board[i+1][j] or board[i][j] == board[i][j+1]: # checks row below and column to the right
+                if board[i][j] == board[i+1][j] or board[i][j] == board[i][j+1]: #checks row below and column to the right
                     not_adj = False
                 elif board[i][j] == board[i-1][j] and i-1>0: # checks row above if row is not 0
                     not_adj = False
